3121938f5baabec6de80ddf6d408c1be66670d6e
Your Name <you@example.com>
2025-09-05 12:21:23 +0500

    feat: add first catalog page

---
+++ scripts/main.js
@@ -0,0 +1,142 @@
+$(document).ready(function () {
+    //sidebar on main page initialization
+    $(".menu").metisMenu();
+    $('.slick-slider').each(function () {
+        let slider = $(this);
+        let options = {
+            prevArrow: slider.parent().find('.slider-prev'),
+            nextArrow: slider.parent().find('.slider-next'),
+            infinite: true,
+            autoPlay: true,
+        }
+        let extraOptions = {}
+        if (slider.hasClass('watch-later')) {
+            extraOptions = {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: false,
+                arrows: true,
+                autoPlay: true,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3.5,
+                            slidesToScroll: 1,
+                            dots: true,
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2.1,
+                            slidesToScroll: 1,
+                            dots: true,
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                        }
+                    },
+                ]
+            }
+        } else if (slider.hasClass('cards')) {
+            extraOptions = {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: false,
+                arrows: true,
+                autoPlay: true,
+                adaptiveHeight: true,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 2.5,
+                            slidesToScroll: 1,
+                            dots: true,
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                            dots: true,
+                            arrows: false,
+                            adaptiveHeight: true,
+                        }
+                    }, {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    },
+                ]
+            }
+        } else if (slider.hasClass('team')) {
+            extraOptions = {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                centerMode: false,
+                arrows: true,
+                autoPlay: true,
+                adaptiveHeight: true,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 2.5,
+                            slidesToScroll: 1,
+                            dots: true,
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                            dots: true,
+                            arrows: false,
+                            adaptiveHeight: true,
+                        }
+                    }, {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    },
+                ]
+            }
+        }
+        slider.slick($.extend({}, extraOptions, options));
+    })
+})
